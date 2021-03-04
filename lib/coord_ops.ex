7ca9a7733447d6f080b8ab589af42a1e0a4474d1
mike sharps <mkshrps@gmail.com>
2021-03-04 13:00:39 +0000

    all primitives and tests complete

--- lib/coord_ops.ex
+++ lib/coord_ops.ex
@@ -4,7 +4,7 @@ defmodule CoordOps do
     a <~> b
   end
 
-  def coord_equal({x,y,z,_},{x1,y1,z1,_}) do
+  def tuple_equal({x,y,z,_},{x1,y1,z1,_}) do
     x <~> x1 and y <~> y1 and z <~> z1
   end
 
@@ -15,7 +15,34 @@ defmodule CoordOps do
   def vector({x,y,z}) do
     {x,y,z,0.0}
   end
+  
+  # t1,t2 4 ele tuples
+  #
+  def add_tuple(t1,t2) do
+    do_op(t1,t2,&(&1 + &2))
+  end
 
+  def multiply_tuple(t1,t2) do
+    do_op(t1,t2,&(&1 * &2))
+  end
+  def subtract_tuple(t1,t2) do
+    do_op(t1,t2,&(&1 - &2))
+  end
+
+  def div_tuple(t1,t2) do
+    do_op(t1,t2,&(&1 / &2))
+  end
+
+  # just pass in the uples and the arithmetic function#
+  # performs function f on matching pairs of elements x,x1 y,y1 etc
+  # accepts 2 tuples
+  # return tuple
+  #
+  def do_op({x,y,z,w},{x1,y1,z1,w1},f) do 
+    {f.(x,x1),f.(y,y1),f.(z,z1),f.(w,w1)}
+  end
+
+  # 
   def negate({x,y,z,w}) do
     {-x,-y,-z,-w}
   end
@@ -24,29 +51,27 @@ defmodule CoordOps do
     {x*s,y*s,z*s,w*s}
   end
 
-  def magnitude({x,y,z}) do
-     sqrt(x*x + y*y + z*z )
+  def magnitude({x,y,z,w}) do
+     sqrt(x*x + y*y + z*z + w*w)
   end
 
-  def add_coord(t1,t2) do
-    do_op(t1,t2,&(&1 + &2))
+  # currently rounding this off to pass test and limit accuracy of magnitude
+  # may not be required 
+  def normalize_vector({x,y,z,w}) do
+    v = magnitude({x,y,z,w}) 
+    {x/v,y/v,z/v,w/v}
+
+  end
+  # t1,t2 are 4 ele tuples
+  def dot(t1,t2) do
+    {x,y,z,w} = multiply_tuple(t1,t2)
+    x+y+z+w
+
   end
 
-  def multiply_coord(t1,t2) do
-    do_op(t1,t2,&(&1 * &2))
-  end
-  def subtract_coord(t1,t2) do
-    do_op(t1,t2,&(&1 - &2))
+  def cross({x,y,z,w},{x1,y1,z1,_w1}) do
+    {(y * z1) - (z * y1),(z * x1) - (x * z1),(x * y1) - (y * x1),w}
   end
 
-  def div_coord(t1,t2) do
-    do_op(t1,t2,&(&1 / &2))
-  end
-
-  # factory function for arithmetic functions on coord tuples
-  # just pass in the uples and the arithmetic function#
-  def do_op({x,y,z,w},{x1,y1,z1,w1},f) do 
-    {f.(x,x1),f.(y,y1),f.(z,z1),f.(w,w1)}
-  end
 
 end
--- test/coordops_test.exs
+++ test/coordops_test.exs
@@ -12,27 +12,43 @@ defmodule CoordTest do
   end
 
   test "adding point to a vector" do
-    assert CoordOps.add_coord({3,-2,5,1},{-2,3,1,0}) == {1,1,6,1}
+    assert CoordOps.add_tuple({3,-2,5,1},{-2,3,1,0}) == {1,1,6,1}
   end
 
   test "subtract two points" do
-    assert CoordOps.subtract_coord(CoordOps.point({3,2,1}), CoordOps.point({5,6,7})) == CoordOps.vector({-2,-4,-6})
+    assert CoordOps.subtract_tuple(CoordOps.point({3,2,1}), CoordOps.point({5,6,7})) == CoordOps.vector({-2,-4,-6})
   end
 
   test "negate tuple" do
     assert CoordOps.negate({1,-2,3,-4}) == {-1,2,-3,4}
   end
   test "magnitude of vector " do
-    assert CoordOps.magnitude({1,0,0}) == 1
-    assert CoordOps.magnitude({0,1,0}) == 1
-    assert CoordOps.magnitude({0,0,1}) == 1
-    assert CoordOps.magnitude({1,2,3}) == sqrt(14)
-    assert CoordOps.magnitude({-1,-2,-3}) == sqrt(14)
+    assert CoordOps.magnitude({1,0,0,0}) == 1
+    assert CoordOps.magnitude({0,1,0,0}) == 1
+    assert CoordOps.magnitude({0,0,1,0}) == 1
+    assert CoordOps.magnitude({1,2,3,0}) == sqrt(14)
+    assert CoordOps.magnitude({-1,-2,-3,0}) == sqrt(14)
   end
+
   test "scale tuple" do
     assert CoordOps.scale({1,-2,3,-4},3.5) == {3.5,-7,10.5,-14}
     assert CoordOps.scale({1,-2,3,-4},0.5) == {0.5,-1,1.5,-2}
   end
-end
+  test "normalize vector" do
+#    assert CoordOps.normalize_vector(CoordOps.vector{1,2,3}) == CoordOps.vector({0.26726,0.53452,0.80178})
+    assert CoordOps.normalize_vector(CoordOps.vector{4,0,0}) == CoordOps.vector({1,0,0})
+    norm =  CoordOps.normalize_vector(CoordOps.vector{1,2,3})
+    assert CoordOps.magnitude(norm) == 1.0
+  end
 
+  test "dot product multiply and add elemnts of 2 tuples" do
+    assert CoordOps.dot(CoordOps.vector({1,2,3}),CoordOps.vector({2,3,4})) == 20
+  end
+
+  test "cross product multiply and add elemnts of 2 tuples" do
+    assert CoordOps.cross({1,2,3,0},{2,3,4,0}) == {-1,2,-1,0}
+    assert CoordOps.cross({2,3,4,0},{1,2,3,0}) == {1,-2,1,0}
+  end
+
+end
 
